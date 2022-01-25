eb58215b061faacb38bd7e62d06c366f116c34c5
Justin Berghahn <justin.berghahn@live.nl>
2022-01-26 00:39:02 +0100

    Added Social

--- about.php
+++ about.php
@@ -11,11 +11,34 @@ require ("./php/head.php");
     require ("./php/nav.php");
     ?>
     <div class="hero-body has-text-centered is-four-fifths">
-        <div class="column ">
-            <h1 class="has-text-white has-text-weight-bold is-size-1 has-text-center ">About<span class="outline ml-4">JUSTIN BERGHAHN</span></h1>
-            <p class="has-text-white">Hello my name is Justin and I am 18 years old, and I live in the Netherlands! I am now following the web development course at the graphic lyceum, in my spare time I also like to make websites! In my spare time I like to go to amusement park around the world with friends! Hopefully I was able to provide some additional information about myself!</p>
+        <div class="columns">
+            <div class="column ">
+                <h1 class="has-text-white has-text-weight-bold is-size-1 has-text-center ">ABOUT<span class="outline ml-4">JUSTIN BERGHAHN</span></h1>
+                <p class="has-text-white">Hello my name is Justin and I am 18 years old, and I live in the Netherlands! I am now following the web development course at the graphic lyceum, in my spare time I also like to make websites! In my spare time I like to go to amusement park around the world with friends! Hopefully I was able to provide some additional information about myself!</p>
+                <div class="columns mt-5">
+                    <div class="column">
+                    </div>
+                    <div class="column">
+                    </div>
+                    <div class="column">
+                        <button class="button is-dark"><a href="https://github.com/OftewelJustin"><i class="fab fa-github has-text-white"></i></a></button>
+                    </div>
+                    <div class="column">
+                        <button class="button is-dark"><a href="https://www.linkedin.com/in/justin-berghahn/"><i class="fab fa-linkedin-in has-text-white"></i></a></button>
+                    </div>
+                    <div class="column">
+                    </div>
+                    <div class="column">
+                    </div>
+                </div>
+            </div>
+
         </div>
+
     </div>
+
+
+
 </section>
 <script>
     $.fakeLoader();
--- index.php
+++ index.php
@@ -17,8 +17,6 @@ require ("./php/head.php");
             <h1 class="has-text-white has-text-weight-bold is-size-1 has-text-center hover-effect-1 "><a href="https://github.com/OftewelJustin">Hi, I'm Justin 👋</a></h1>
             <p class="has-text-white has-text-weight-bold is-size-4 has-text-center hover-effect-1">Welcome to my Portfolio!</p>
         </div>
-
-
     </div>
     <script>
         $.fakeLoader();
